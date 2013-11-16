2ec1f78680b8bb4eb0767d9c274d98109546ac41
adamsar <adamsar@gmail.com>
2013-11-16 19:25:46 +0900

    Get rid of Vagrant

--- vagrant/aws-dummy/site-cookbooks/base/recipes/app.rb
+++
@@ -1,7 +0,0 @@
-directory "/var/log/gyro-n" do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-end
-
--- vagrant/aws-dummy/site-cookbooks/base/recipes/tomcat_app.rb
+++
@@ -1,9 +0,0 @@
-directory "/var/log/gyro-n" do
-  owner "tomcat"
-  group "tomcat"
-  mode 00755
-  action :create
-end
-
-
-
