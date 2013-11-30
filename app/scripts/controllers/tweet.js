abc0f01d8845c88a63f54f10b8a0c6c999fd9a2f
adamsar <adamsar@gmail.com>
2013-11-30 22:04:15 +0900

    Twit pic

--- app/scripts/controllers/tweet.js
+++ app/scripts/controllers/tweet.js
@@ -1,11 +1,55 @@
 'use strict';
 angular.module('kanjouMapApp')
-    .controller('TweetCtrl', function($scope, colors){
-	$scope.$parent.page = 'tweet';
-	$scope.colors = colors;
-	$scope.currentEmotion = colors.colorKeys[0];
-	$scope.setEmotion = function(emotion){
-	    $scope.currentEmotion = emotion;
-	}
+.controller('TweetCtrl', function($scope, colors){
+  $scope.$parent.page = 'tweet';
+  $scope.colors = colors;
+  $scope.currentEmotion = colors.colorKeys[0];
+  $scope.currentTweets = [];
+  $scope.tweets = {};
 
+  $scope.setTweets = function(){
+      $scope.currentTweets = $scope.tweets[$scope.currentEmotion];
+  }
+
+  $scope.setEmotion = function(emotion){
+    $scope.currentEmotion = emotion;
+    $scope.setTweets();
+  }
+
+  function blankTweets(){
+    $scope.tweets = {};
+    _.each(colors.colorKeys, function(emotion){
+      $scope.tweets[emotion] = [];
     });
+  }
+
+  blankTweets();
+
+  $scope.organizeTweets = function(){
+    _.each($scope.data, function(item){
+      var strongest = colors.getStrongest(item.kanjoData);
+      var key = colors.getEmotionName(strongest, item.kanjoData[strongest]);
+      $scope.tweets[key].push({
+        _id: item._id,
+        name: item.name,
+        text: item.text,
+        profile_pic: item.profile_pic,
+        pic: item.pic,
+        value: Math.abs(item.kanjoData[strongest])});
+    });
+
+    _.each(colors.colorKeys, function(emotion){
+      $scope.tweets[emotion] = _.sortBy($scope.tweets[emotion],
+                                        function(tweet){return tweet.value;});
+    });
+    $scope.$broadcast("tweetsChanged");
+  }
+
+  $scope.$on("tweetsChanged", $scope.setTweets);
+  $scope.$on("dataBlank", blankTweets);
+  $scope.$on("dataRefreshed", $scope.organizeTweets);
+
+  if($scope.started){
+    $scope.organizeTweets();
+  }
+});
--- app/scripts/directives/tweet_box.js
+++ app/scripts/directives/tweet_box.js
@@ -1,55 +1,13 @@
 angular.module('kanjouMapApp')
-    .directive('tweetBox', function(){
-	return {
-	    scope: true,
-	    templateUrl: "/views/partials/tweet-box.html",
-	    controller: function($scope, colors){
-		$scope.currentTweets = [];
-		$scope.tweets = {};
-		function blankTweets(){
-		    $scope.tweets = {};
-		    _.each(colors.colorKeys, function(emotion){
-			$scope.tweets[emotion] = [];
-		    });		    
-		}
-		blankTweets();
-		
-		$scope.organizeTweets = function(){
-		    _.each($scope.data, function(item){
-			var strongest = colors.getStrongest(item.kanjoData);
-			var key = colors.getEmotionName(strongest, item.kanjoData[strongest]);
-			$scope.tweets[key].push({
-                _id: item._id,
-				name: item.name,
-	    		text: item.text,
-				profile_pic: item.profile_pic,
-                pic: item.pic,
-				value: Math.abs(item.kanjoData[strongest])});
-		    });
-		    _.each(colors.colorKeys, function(emotion){
-			$scope.tweets[emotion] = _.sortBy($scope.tweets[emotion], 
-							  function(tweet){
-							      return tweet._id;
-							  });			
-		    });
-            console.debug(_.filter($scope.tweets['joy'], function(tweet){
-                return tweet.pic != null;
-            })
-            );
-		    $scope.$broadcast("tweetChange");
-		}
-		$scope.$on("dataBlank", blankTweets);
-		$scope.$on("dataRefreshed", $scope.organizeTweets);
-		$scope.$watch("currentEmotion", function(newEmotion){
-		    $scope.currentTweets = $scope.tweets[newEmotion];
-		});
-		$scope.$on("tweetChange", function(){
-		    $scope.currentTweets = $scope.tweets[$scope.currentEmotion];
-		});
-		$scope.currentTweets = $scope.tweets[$scope.currentEmotion];
-		if($scope.started){
-		    $scope.organizeTweets();
-		}
-	    }
-	};
-    });
+.directive('tweetBox', function(){
+  return {
+    scope: {
+      "tweets": "="
+    },
+    templateUrl: "/views/partials/tweet-box.html",
+
+    controller: function($scope){
+    },
+
+  };
+});
